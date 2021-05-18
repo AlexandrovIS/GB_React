e4dc1f3a4668d073ebfb552e6a899e4349d67895
Ivan <alexandrovis@gmail.com>
2021-05-18 12:46:21 +0300

    hw_1

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// import { a } from './home'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+
+class ClassExample extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Hello Class
+        </h2>
+      </div>
+    )
+  }
+}
+
+const Example = () => {
+  return (
+    <div>
+      <h1>
+        Hello react
+      </h1>
+      <ClassExample />
+    </div>
+  )
+}
+
+const Message = ({ message }) => {
+  return (
+    <React.Fragment>
+      <h2>{message}</h2>
+    </React.Fragment>
+  )
+}
+
+const MessageField = ({ message }) => {
+  return message.map((message, index) => <Message message={message} key={index} />)
+}
+
+ReactDom.render(<MessageField message={['Message']} />, document.getElementById('root'))
